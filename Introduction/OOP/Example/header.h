b90f30732e3dbc79abc1b54d005fc0130771b393
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2022-12-19 18:19:01 +0700

    Update private function call

--- Introduction/OOP/Example/header.h
+++ Introduction/OOP/Example/header.h
@@ -9,10 +9,15 @@ class exampleClass
   	public:
 		static int 	static_var;
 		static void read_static_var();
+		static int 	return_int_num();
+		static void print_argument(int argument);
 		int 		instance_var;
 		void 		class_constructor();
 		void 		add_number(int int_param);
+		void 		call_private_function();
+		
   	private:  
     	int 		_int_param;
+		void		display_string();
 };
 #endif
