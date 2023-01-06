c6dc973442b0ecfba7f3789adc82715da06012da
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2023-01-06 11:43:57 +0700

    Add important comment for function calling

--- Introduction/OOP/Example/main.cpp
+++ Introduction/OOP/Example/main.cpp
@@ -1,13 +1,13 @@
 #include "header.h"
 
-main(){
+int main(){
 	exampleClass object;
 	object.class_constructor();
     object.add_number(13);
 	object.read_static_var();
 	object.call_private_function();
-    object.call_object_method(object);
+    object.call_object_method(object);//Call function with the same object
 
-    exampleClass object_1;
-    object.call_object_method(object_1);
+    exampleClass new_object;
+    object.call_object_method(new_object);//Call function with the new object
 }
