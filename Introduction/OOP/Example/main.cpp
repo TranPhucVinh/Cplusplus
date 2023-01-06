73cf32d3b6db2e710a6845bda1f7646678067762
TranPhucVinh <30307543+TranPhucVinh@users.noreply.github.com>
2023-01-06 11:40:41 +0700

    Add function to call with object as parameter

--- Introduction/OOP/Example/main.cpp
+++ Introduction/OOP/Example/main.cpp
@@ -6,4 +6,8 @@ main(){
     object.add_number(13);
 	object.read_static_var();
 	object.call_private_function();
+    object.call_object_method(object);
+
+    exampleClass object_1;
+    object.call_object_method(object_1);
 }
