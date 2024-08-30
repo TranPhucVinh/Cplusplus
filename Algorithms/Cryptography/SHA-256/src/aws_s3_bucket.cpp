40267a22a7205a7ba938794f964832b63b89aa11
vinh.tran <tranphucvinh96@gmail.com>
2024-08-30 05:53:17 -0400

    HTTP request for AWS S3 bucket

--- Algorithms/Cryptography/SHA-256/src/aws_s3_bucket.cpp
+++ Algorithms/Cryptography/SHA-256/src/aws_s3_bucket.cpp
@@ -19,10 +19,10 @@ using namespace std;
 string access_key_id, secret_access_key, session_key;
 
 void    get_aws_env_vars(string &access_key_id, string &secret_access_key, string &session_key);
-string  sha_256_to_string(uint32_t* sha_256_hash);
+string  sha_256_to_string(unique_ptr<uint32_t[]> sha_256_hash);
 char   *amz_date();
 string  yyyymmdd();
-string  form_canon_req(const char *host, uint32_t* payload_hash, char *amz_date, const char *method,
+string  form_canon_req(const char *host, string payload_hash, char *amz_date, const char *method,
                     const char *uri, const char *querystring);
 string  form_string_to_sign(const char *string_to_sign, char *amz_date, const char *region, string canon_req);
 string  calculate_signature(string secret_access_key, const char *region);
@@ -40,10 +40,10 @@ class HTTP_Client {
 int main() {
     string msg = "";
     SHA256 sha256, hmac;
-    uint32_t* payload_hash = sha256.hex_digest(msg);
 
+    string sha_256_msg_str = sha_256_to_string(move(sha256.hex_digest(msg)));
     get_aws_env_vars(access_key_id, secret_access_key, session_key);
-    string cq = form_canon_req(HOST, payload_hash, amz_date(), "GET", "/", "");
+    string cq = form_canon_req(HOST, sha_256_msg_str, amz_date(), "GET", "/", "");
     string SigningKey = calculate_signature(secret_access_key, REGION);
     string StringToSign = form_string_to_sign(STRING_TO_SIGN, amz_date(), REGION, cq);
 
@@ -54,7 +54,7 @@ int main() {
     string http_request;
 
     http_request = "GET / HTTP/1.1\r\nHost: " + string(HOST);
-	http_request += "\r\nx-amz-content-sha256: " + sha_256_to_string(payload_hash);
+	http_request += "\r\nx-amz-content-sha256: " + sha_256_msg_str;
     http_request += "\r\nx-amz-date: " + string(amz_date());
     http_request += "\r\nx-amz-security-token: " + session_key;
     http_request += "\r\nAuthorization: AWS4-HMAC-SHA256 Credential=" + Credential;
@@ -104,26 +104,24 @@ string yyyymmdd() {
     return string(buffer);
 }
 
-string form_canon_req(const char *host, uint32_t* payload_hash, char *amz_date, 
+string form_canon_req(const char *host, string payload_hash, char *amz_date, 
         const char *method, const char *uri, const char *querystring) {
     
-    string cq = "", payload_hash_str;
-
-    payload_hash_str = sha_256_to_string(payload_hash);
+    string cq = "";
 
     cq += string(method) + "\n" + string(uri);
     cq += "\n" + string(querystring) + "\n";
 
     // Canonical Headers
     cq += "host:" + string(host) + "\n";
-    cq += "x-amz-content-sha256:" + payload_hash_str + "\n";
+    cq += "x-amz-content-sha256:" + payload_hash + "\n";
     cq += "x-amz-date:" + string(amz_date) + "\n";
 
     // Signed Headers
     cq += "\nhost;x-amz-content-sha256;x-amz-date\n";
 
     // Payload Hash
-    cq += payload_hash_str;
+    cq += payload_hash;
 
     return cq;
 }
@@ -151,17 +149,21 @@ string calculate_signature(string secret_access_key, const char *region) {
 }
 
 void get_aws_env_vars(string &access_key_id, string &secret_access_key, string &session_key) {
-    access_key_id = std::getenv("AWS_ACCESS_KEY_ID");
-    secret_access_key = std::getenv("AWS_SECRET_ACCESS_KEY");
-    session_key = std::getenv("AWS_SESSION_TOKEN");
+    const char* access_key_id_cstr = getenv("AWS_ACCESS_KEY_ID");
+    const char* secret_access_key_cstr = getenv("AWS_SECRET_ACCESS_KEY");
+    const char* session_key_cstr = getenv("AWS_SESSION_TOKEN");
 
-    if (access_key_id == "") {
+    if (access_key_id_cstr == nullptr) {
         cout << "There is no AWS_ACCESS_KEY_ID\n";
         exit(0);
+    } else {
+        access_key_id = string(access_key_id_cstr);
+        secret_access_key = string(secret_access_key_cstr);
+        session_key = string(session_key_cstr);
     }
 }
 
-string sha_256_to_string(uint32_t* sha_256_hash) {
+string sha_256_to_string(unique_ptr<uint32_t[]> sha_256_hash) {
     string _sha256_str = "";
     for (int i = 0; i < 8; i++) {
         stringstream _stream;
