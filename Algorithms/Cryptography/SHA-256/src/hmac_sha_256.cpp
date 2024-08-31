5dfc0613e386e09f8839c7997620f3f2230f6edb
vinh.tran <tranphucvinh96@gmail.com>
2024-08-31 12:00:37 -0400

    HMAC SHA-256 with key is 64 bytes

--- Algorithms/Cryptography/SHA-256/src/hmac_sha_256.cpp
+++ Algorithms/Cryptography/SHA-256/src/hmac_sha_256.cpp
@@ -15,15 +15,16 @@ string sha_256_to_string(unique_ptr<uint32_t[]> sha_256_hash) {
 }
 
 int main() {
-    string msg1 = "", msg2 = "msg";
+    string msg1 = "", msg2 = "msg", msg3 = "key_64_chars";
 
     string key = "key";
-    
+    string key_64_chars = "0b2c9cc2bb3ce0b8f92db192dadb06e60978338051ece09542f34a865a7d3717";
+
     SHA256 obj;// Object for HMAC-SHA256
     unique_ptr<uint32_t[]> hmac_sha256_hash = obj.hmac_sha_256(key, msg1);
 
     cout << sha_256_to_string(move(hmac_sha256_hash)) << endl;
     cout << sha_256_to_string(obj.hmac_sha_256(key, msg2)) << endl;
-
+    cout << sha_256_to_string(obj.hmac_sha_256(key_64_chars, msg3)) << endl;
     return 0;
 }
